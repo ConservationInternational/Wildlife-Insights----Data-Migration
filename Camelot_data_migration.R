8f0083c9632fc762cd42ed51bd7c87547c350056
efegraus@conservation.org <efegraus@conservation.org>
2019-03-14 10:40:32 -0700

    Setting up the script with comments and objectives

---
+++ Camelot_data_migration.R
@@ -0,0 +1,10 @@
+# Camelot to Wildlife Insights Data Migration Tool
+# We are starting this based on a dataset from WWF Canada
+# Goal: Build a mapping from Camelot export (.csv file) into 
+# WI Batch Upload Template
+# 1. Do the mappings
+# 2. Fill in data attributes as needed.
+# 3. Load the Batch Upload Template and conduct QA/QC (need to define.)
+
+
+#LOAD WWF CANADA DATA

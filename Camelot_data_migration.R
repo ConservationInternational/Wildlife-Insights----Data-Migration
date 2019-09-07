161d2b528c554b0ead894fd9e0f44ccac48669e2
efegraus@conservation.org <efegraus@conservation.org>
2019-09-06 17:00:16 -0700

    Many changes to accomodate new functions as well as final changes to teh batch upload templates.

--- Camelot_data_migration.R
+++ Camelot_data_migration.R
@@ -1,9 +1,6 @@
 # Camelot_data_migration.R Eric Fegraus 4/17/2019
 # Purpose: Camelot to Wildlife Insights Data Migration Tool
-# ARNO- How was the data exported out of camelot?
-# We are starting this based on a dataset from WWF Canada
-# Goal: Build a mapping from Camelot export (.csv file) into 
-# WI Batch Upload Template
+# This will also serve as a template for other data formats.
 
 ######
 # Clear workspace
@@ -14,201 +11,168 @@ rm(list=ls())
 library(dplyr)
 library(googlesheets)
 library(jsonlite)
+source('wi_functions.R')
+
 ######
 # Load your data and type in any information that cannot be captured from the dataset directly. 
 ct_data <- read.csv("data/South Chilcotins Wildlife Suvey.csv")
 # Establish any variables needed for each project (i.e. not found in the datafile)
 
-#PROJECT METADATA- UPDATE ALL INFORMATION HERE
-# Setting up many variables that are not found in the dataset itself.
-PrjName<-"South Chilcotins Wildlife Survey 2018"
-PrjObj <-"Comparison of camera trapping vs. eDNA mammal abundance estimates"
-PrjSpecies <-"Multiple" # Multiple or Single Species
-PrjSpeciesInd <- NA # If single list out the species (Genus species and comma separated)
-PrjSensorLayout <- "Systematic" # Options:Systematic, Randomized, Convenience,  Targeted 
-PrjSensorLayoutTarget <- NA
-PrjBaitUse <- "No"  #Was bait used? Options: Yes,Some,No
-PrjBaitUseType <- NA
-PrjStrata <- "No" #Options: Yes, No
-PrjStrataType <- NA
-PrjSensorMethod <- "Sensor detection"
-PrjIndAnimals <- "No" #Options: Yes, No
-PrjBlankImages <- "Yes" # Were blanks removed? Options: Yes, No
-PrjSensorCluster <- "No"
-PrjAdmin <- "Robin Naidoo"
-PrjAdminEmail <- "Robin.Naidoo@wwfus.org"
-PrjAdminOrg <- "WWF" # This should be pulled from WI database (if the organization is already registered)
-PrjCC <- "CAN" #DEV OPTION: Enter in three letter ISO code.  Consider building a function to find three leter ISO country code
-PrjEmbargo <- 24
-
-# CAMERA QUESTIONS
-# For now we will assume we will get Make and Model information from EXIF reader upon data ingestion into WI.
-# DEV OPTION: Consider building into this with exifr.
-#DEPLOYMENT QUESTIONS
-DepBait <- PrjBaitUse # Was bait used? Options: Yes, some, No.  Connect this to PrjBait...need a way to assign this if answer = "some".
-  # Dataset question: Trap station ID - what is it?
-  # Dataset question: Trap Station Session Camera ID ..what is it?
-  # Dataset question: Relationship of Camera Name with Camera ID?
-  #Dataset question: Quiet Period and Camera Failure Details are required. Are these in the dataaset?
-
-#IMAGE QUESTIONS
-# Change the file path names for your images. Supply what your original path (original_path) with a replacement string (sub_path)
-#original_path <- dQuote(D:\N\personal\investments\3909 Gun Creek Road\research\Camelot_DB\Media)
-#sub_path <- "D:/BritishColumbia-CT"
-# If all images were identified by one person, set this here. Otherwise comment this out.
-image_identified_by <- " Robin Naidoo"
-  # Dataset question: Location - tell me what to limit the string and i'll include this into the script. It will be useful in other situations.
-  # Dataset question: Photo_Type_Identified_by - this is required. Can you find out this person(s)? Alternative is we make it not required.
-  # Count: This should be one record per species with counts of those animals.
-  # Date: We will need to reformat this prior to upload.
-
-
-
-######################
-# Move everything below into a function
-######################
-# Create and write out the batch upload templates as csv's: Project, Camera, Deployment, Images
-# Load the template-- Might use this for QA/QC or for creating the final data frames that we write out.
-# Or maybe not...TBD.
-wi_batch<- gs_url("https://docs.google.com/spreadsheets/d/1PE5Zl-HUG4Zt0PwSfj-gJRJVbZ__LgH3VuiDW3-BKQg", visibility = "public")
-project_batch <- wi_batch %>% gs_read_csv(ws="Projectv1.0")
-prj_df_colnames <- project_batch$`Form Value`
-prj_df_colnames <- gsub(" ","_",prj_df_colnames)
-prj_dff <- data.frame(matrix(ncol = length(prj_df_colnames),nrow=1))
-colnames(prj_dff) <- prj_df_colnames 
-#prj_dff is now an empty dataframe
-
-#Camerav1.0
-cam_batch <- wi_batch %>% gs_read_csv(ws="Camerav1.0")
-cam_df_colnames <- cam_batch$`Form Value`
-cam_df_colnames <- gsub(" ","_",cam_df_colnames)
-# Make cam_df_length unique to the number of cameras in the project
-cam_df_length <- length(unique(ct_data$Camera.ID)) # or use nrow
-cam_dff <- data.frame(matrix(ncol = length(cam_df_colnames),nrow=cam_df_length))
-colnames(cam_dff) <- cam_df_colnames 
-
-#Deploymentv1.0
-dep_batch <- wi_batch %>% gs_read_csv(ws="Deploymentv1.0")
-dep_df_colnames <- dep_batch$`Form Value`
-dep_df_colnames <- gsub(" ","_",dep_df_colnames)
-# Make dep_df_length unique to the number of cameras in the project
-dep_df_length <- length(unique(paste(ct_data$Site.Name,ct_data$Session.Start.Date,ct_data$Session.End.Date,sep="-")))
-dep_dff <- data.frame(matrix(ncol = length(dep_df_colnames),nrow=dep_df_length))
-colnames(dep_dff) <- dep_df_colnames 
-
-#Imagev1.0
-image_batch <- wi_batch %>% gs_read_csv(ws="Imagev1.0")
-image_df_colnames <- image_batch$`Form Value`
-image_df_colnames <- gsub(" ","_",image_df_colnames)
-# Set number of rows to full dataset.
-image_df_length <- nrow(ct_data)
-image_dff <- data.frame(matrix(ncol = length(image_df_colnames),nrow=image_df_length))
-colnames(image_dff) <- image_df_colnames 
 ######
-# Create each batch upload template
-# Project .csv template
-prj_dff$Project_ID <- unique(ct_data$Survey.Name)
-prj_dff$Project_Name <- PrjName  
-prj_dff$Project_Objectives <- PrjObj
-prj_dff$Project_Species <- PrjSpecies
-prj_dff$Project_Species_Individual  <- PrjSpeciesInd
-prj_dff$Project_Sensor_Layout <- PrjSensorLayout
-prj_dff$Project_Sensor_Layout_Targeted <- PrjSensorLayoutTarget
-prj_dff$Project_Bait_Use <- PrjBaitUse
-prj_dff$Project_Bait_Type <- PrjBaitUseType
-prj_dff$Project_Stratification <- PrjStrata
-prj_dff$Project_Stratification_Type <- PrjStrataType
-prj_dff$Project_Sensor_Method <- PrjSensorMethod
-prj_dff$Project_Individual_Animals <- PrjIndAnimals
-prj_dff$Project_Blank_Images <- PrjBlankImages
-prj_dff$Project_Sensor_Cluster <- PrjSensorCluster
-prj_dff$Project_Admin <- PrjAdmin
-prj_dff$Project_Admin_Email <- PrjAdminEmail 
-prj_dff$Project_Admin_Organization <- PrjAdminOrg
-prj_dff$Country_Code <- PrjCC
-prj_dff$Embargo_Period <- PrjEmbargo
+# Project Batch Upload Template: Load in the project batch upload template and fill it out.
+prj_bu <- wi_batch_function("Project",dep_length)
+
+# Many of the project variables may not be found in your dataset. If you can get them from your
+# data great! Otherwise type them in here. 
+prj_bu$project_id <- unique(ct_data$Survey.ID)
+prj_bu$project_name <- "South Chilcotins Wildlife Survey 2018"
+prj_bu$project_objectives <- "Comparison of camera trapping vs. eDNA mammal abundance estimates"
+prj_bu$project_species <- "Multiple" # Multiple or Single Species
+prj_bu$project_species_individual  <- NA # If single list out the species (Genus species and comma separated)
+prj_bu$project_sensor_layout <- "Systematic" # Options:Systematic, Randomized, Convenience,  Targeted 
+prj_bu$project_sensor_layout_targeted_type <-  NA
+prj_bu$project_bait_use <- "No"  #Was bait used? Options: Yes,Some,No
+prj_bu$project_bait_type <- NA
+prj_bu$projection_stratification <- "No" #Options: Yes, No
+prj_bu$projection_stratification_type <- NA
+prj_bu$project_sensor_method <- "Sensor detection"
+prj_bu$project_individual_animals <- "No" #Options: Yes, No
+prj_bu$project_blank_images <- "Yes" # Were blanks removed? Options: Yes, No
+prj_bu$project_sensor_cluster <- "No"
+prj_bu$project_admin <- "Robin Naidoo"
+prj_bu$project_admin_email <- "Robin.Naidoo@wwfus.org"
+prj_bu$project_admin_organization <- "WWF" # 
+prj_bu$country_code <- "CAN"
+prj_bu$embargo <- 0 # 0-24 months
+prj_bu$metadata_license <- "CC-BY" # Two options: CC0,CC-BY
+prj_bu$image_license <- "CC-BY-NC" # Three options: CC0,CC-BY,CC-BY-NC
+
+
 
 ######
-# Camera .csv template
-cam_dff$Project_ID <- unique(prj_dff$Project_ID) # If more than one error for now
-cam_dff$Camera_ID <- unique(ct_data$Camera.ID)
+# Camera Batch Upload Template: Fill in the information relatd to the cameras/sensors used in your project
+# First need to get the number of cameras used in the project
+num_sensors <- length(unique(ct_data$Camera.ID))
+# Get the empty Camera template
+cam_bu <- wi_batch_function("Camera",num_sensors)
+# Fill out each Camera field
+cam_bu$project_id <- unique(prj_bu$project_id) # If more than one error for now
+cam_info <- distinct(ct_data,Camera.ID,Make,Model)
+cam_bu$camera_id <- cam_info$Camera.ID
+cam_bu$make <- cam_info$Make
+cam_bu$model <- cam_info$Model
+# If serial number and year purchased are available add them in as well.
+cam_bu$serial_number <- MA
+cam_bu$year_purchased <- NA
+# Notes: We will also try to get information from image EXIF data upon data ingestion into WI. 
 
 ######
-# Deployment .csv template - TODO - Review all of these to see what else will map?
+# Deployment Batch Upload Template: Fill in the information related to each deployment. A deployment is a sensor 
+# observing wildlife for some amount of time in a specific location. 
+# 
 # 1. Establish unique deployments - Should be Site.Name + pair(SessionStart.Date--> Session.End.Date)
 ct_data$deployments <- paste(ct_data$Site.Name,ct_data$Session.Start.Date,ct_data$Session.End.Date,sep="-")
 # 2. Create a distinct dataframe based on deployments
 dep_temp<-distinct(ct_data,deployments,.keep_all = TRUE )
-# 3. Create the final dataframe
-dep_dff$Project_ID <- unique(prj_dff$Project_ID) # If more than one error for now
-dep_dff$Deployment_ID <- dep_temp$deployments
-dep_dff$Deployment_Location__ID  <- dep_temp$Site.Name
-dep_dff$Longitude <- dep_temp$Camelot.GPS.Longitude
-dep_dff$Latitude <- dep_temp$Camelot.GPS.Latitude
-dep_dff$Camera_Deployment_Begin_Date <- dep_temp$Session.Start.Date
-dep_dff$Camera_Deployment_End_Date <- dep_temp$Session.End.Date
-dep_dff$Event
-dep_dff$Array_Name
-dep_dff$Bait_Type <- DepBait
-dep_dff$Bait_Description
-dep_dff$Feature_Type
-dep_dff$Feature_Type_Methodology
-dep_dff$Camera_ID <- dep_temp$Camera.ID
-dep_dff$Quiet_Period_Setting
-dep_dff$Camera_Failure_Details
-dep_dff$Altitude
-dep_dff$Height
-dep_dff$Height_Other
-dep_dff$Angle
-dep_dff$Angle_Other
+# 3. Get the empty deployement dataframe
+dep_bu <- wi_batch_function("Deployment",nrow(dep_temp))
+# 4. Fill it in
+dep_bu$project_id <- unique(prj_bu$project_id) # If more than one error for now
+dep_bu$deployment_id <- dep_temp$deployments
+dep_bu$placename <- dep_temp$Site.Name
+dep_bu$longitude <- dep_temp$Camelot.GPS.Longitude
+dep_bu$latitude <- dep_temp$Camelot.GPS.Latitude
+dep_bu$start_date <- dep_temp$Session.Start.Date
+dep_bu$end_date <- dep_temp$Session.End.Date
+dep_bu$event <- NA
+dep_bu$array <- NA
+dep_bu$bait_type <- "No" # Note that if bait was ussed but it was not consisten across all deployments, this is where you enter it. 
+    # Logic may be needed to figure out which deployments had bait and which didn't. Similar thing if bait type was vaired in deployments.
+    # Options: Yes, some, No.  We may need a way to assign this if answer = "some".
+dep_bu$bait_description <- NA
+dep_bu$feature_type <- NA
+dep_bu$feature_type_methodology <- NA
+dep_bu$camera_id <- dep_temp$Camera.ID
+dep_bu$quiet_period  <- NA
+dep_bu$camera_functioning  <- NA
+dep_bu$sensor_height  <- NA
+dep_bu$height_other  <- NA
+dep_bu$sensor_orientation  <- NA
+dep_bu$orientation_other  <- NA
+dep_bu$recorded_by <- NA
 
 
-# Import the wi_taxonomy dataset.
-wi_taxa <- fromJSON("https://staging.api.wildlifeinsights.org/api/v1/taxonomy?fields=class,order,family,genus,species,commonNameEnglish&taxon_level=species&page[size]=3000")
-wi_taxa_data <- wi_taxa$data
-wi_taxa <- readRDS(file = "wi_datafiles/wi_taxonomy.rds")
-
-
-
-con <- curl("https://staging.api.wildlifeinsights.org/api/v1/taxonomy?fields=class,order,family,genus,species,commonNameEnglish&taxon_level=species&page[size]=3000")
-readLines(con)
-
-project_unique_species <- as.data.frame(unique(paste(ct_data$Species,ct_data$Species.Common.Name)))
-# Write out a .csv file that the data provider will use to map into the WI taxonomic authority.
-# Set number of rows to full dataset.
-prj_species_df_length <- nrow(project_unique_species)
-#image_dff <- data.frame(matrix(ncol = 1,nrow=prj_species_df_length)) # Add in the colnames: lookup, Species, Common, WI Genus, WI Species, WI Common OR JUST WI TAXONOMY ID
-#colnames(image_dff) <- image_df_colnames 
-write.csv(project_unique_species,file = "WI_batch_species_lookup.csv")
+######
+# Image Batch Upload Template: Fill in the information related to each image
+# 
+# 1. Do data modifications if needed here.
+# Change the file path names for your images. Supply what your original path (original_path) with a replacement string (sub_path)
+# original_path <- dQuote(D:\N\personal\investments\3909 Gun Creek Road\research\Camelot_DB\Media)
+#  sub_path <- "D:/BritishColumbia-CT"
+# If all images were identified by one person, set this here. Otherwise comment this out.
+image_identified_by <- " Robin Naidoo"
+  
+# 2. Load in the Image batch upload template
+image_bu <- wi_batch_function("Image",nrow(ct_data))
 
 ######
 # Image .csv template
-image_dff$Project_ID <- prj_dff$Project_ID
-image_dff$Deployment_ID <- ct_data$deployments
-image_dff$Image_ID <- ct_data$Media.Filename
-image_dff$Location <- ct_data$Absolute.Path # Modify this to let user sub in new path.
-image_dff$Blank[which(ct_data$Genus == "")] <-1 # Expand as needed as we look at more datasets from Camelot.
-image_dff$Photo_Type_Identified_by <- image_identified_by
-# Build out more taxonomic information as needed here.
-image_dff$Genus_Species <- ct_data$Species # Lots of quality control to do here.
-image_dff$Species_Common_Name <- ct_data$Species.Common.Name  # Lots of quality control to do here.
-image_dff$Uncertainty <- NA
-image_dff$Taxonomic_Authority_or_Source  <- NA # potentially delete
-image_dff$Date_Time_Captured  <- ct_data$Date.Time
-image_dff$Age <- ct_data$Life.stage
-image_dff$Sex <- ct_data$Sex
-image_dff$Animal_recognizable <- NA
-image_dff$Individual_ID <- NA
-image_dff$Individual_Animal_Notes <- NA
-image_dff$Image_Favorite <- NA
-image_dff$Color <- ct_data$Colour
+image_bu$project_id<- prj_bu$project_id
+image_bu$deployment_id <- ct_data$deployments
+image_bu$image_id <- ct_data$Media.Filename
+image_bu$location <- ct_data$Absolute.Path # Modify this to let user sub in new path.
+image_bu$is_blank[which(ct_data$Genus == "")] <-1 # Expand as needed as we look at more datasets from Camelot.
+image_bu$identified_by <- image_identified_by
+# Build out more taxonomic information as needed here ASAP. Will be done the week of September 9
+image_bu$wi_taxon_id
+image_bu$class
+image_bu$order
+iamge_bu$family
+image_bu$genus
+image_bu$species
+image_bu$common_name
+image_bu$uncertainty
+image_bu$timestamp <- ct_data$Date.Time
+image_bu$age <- ct_data$Life.stage
+image_bu$sex <- ct_data$Sex
+image_bu$animal_recognizable <- NA
+image_bu$number_of_animals <- 1
+image_bu$individual_animal_notes <- NA
+image_bu$highlighted <- NA
+image_bu$color <- ct_data$Colour
+
+# Write out the 4 csv files for required for Batch Upload
+# Get a clean site name first - no whitespaces
+site_name_clean <- gsub(" ","_",prj_bu$project_name)
+
+write.csv(prj_bu,file=paste(site_name_clean,"_project.csv",sep=""))
+write.csv(cam_bu,file=paste(site_name_clean,"_camera.csv",sep=""))
+write.csv(dep_bu,file=paste(site_name_clean,"_deployment.csv",sep=""))
+write.csv(image_bu,file=paste(site_name_clean,"_image.csv",sep=""))
+
+###########
+# Misc things we are considering.
+# Dataset question: Photo_Type_Identified_by - this is required. Can you find out this person(s)? Alternative is we make it not required.
+# Count: This should be one record per species with counts of those animals.
+# Date: Need to check WI validation on date types. 
 
 
-######
-# Write out the 4 csv files for batch upload
-write.csv(prj_dff,file=paste(PrjName,"_project.csv",sep=""))
-write.csv(cam_dff,file=paste(PrjName,"_camera.csv",sep=""))
-write.csv(dep_dff,file=paste(PrjName,"_deployment.csv",sep=""))
-write.csv(image_dff,file=paste(PrjName,"_image.csv",sep=""))
+# Read in the Wildlfie Insights Global Taxonomy
+# wi_taxa <- fromJSON("https://api.wildlifeinsights.org/api/v1/taxonomy?fields=class,order,family,genus,species,taxonomyType,uniqueIdentifier,commonNameEnglish&page[size]=30000")
+# wi_taxa_data <- wi_taxa$data
+# wi_taxa_data <- wi_taxa_data %>% replace(., is.na(.), "")
+# project_unique_species <- as.data.frame(unique(paste(ct_data$Species,ct_data$Species.Common.Name)))
+# # Write out a .csv file that the data provider will use to map into the WI taxonomic authority.
 
+taxa_data <- distinct(ct_data,Class,Order,Family,Genus,Species.ID,Species,Species.1)
+write.csv(taxa_data,"taxa_data.csv")
+#
+
+# # Set number of rows to full dataset.
+# prj_species_df_length <- nrow(project_unique_species)
+# #image_dff <- data.frame(matrix(ncol = 1,nrow=prj_species_df_length)) # Add in the colnames: lookup, Species, Common, WI Genus, WI Species, WI Common OR JUST WI TAXONOMY ID
+# #colnames(image_dff) <- image_df_colnames 
+# write.csv(project_unique_species,file = "WI_batch_species_lookup.csv")
+#image_bu$Genus_Species <- ct_data$Species # Lots of quality control to do here.
+#image_bu$Species_Common_Name <- ct_data$Species.Common.Name  # Lots of quality control to do here.
 
